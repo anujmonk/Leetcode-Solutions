5438bb60e3b43c8640cb7f27f5eba65f19c69efc
anujmonk <74954662+anujmonk@users.noreply.github.com>
2020-11-28 18:43:56 +0530

    Update check-if-n-and-its-double-exist.java

--- check-if-n-and-its-double-exist.java
+++ check-if-n-and-its-double-exist.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+     // Will need to learn how to implement HashMap or HashSet in Java to decrese Runtime. 
+     
         boolean check=false;
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
