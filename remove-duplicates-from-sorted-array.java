9d85ec3f527aa50c369ac944596a85310ad4c961
anujmonk <74954662+anujmonk@users.noreply.github.com>
2020-12-03 20:04:42 +0530

    Update remove-duplicates-from-sorted-array.java

--- remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array.java
@@ -5,6 +5,23 @@ class Solution {
         else if(nums.length==1)
             return 1;
         else{
+         /*
+         This can be solved using two pointer technique so that Time Complexity is O(n).
+         Only else block will change.
+         
+         else{
+                int write_pointer=1,read_pointer=1;
+                for(;read_pointer<nums.length;read_pointer++){
+                    if(nums[read_pointer]!=nums[read_pointer-1]){
+                        
+                        nums[write_pointer]=nums[read_pointer];
+                        write_pointer++;
+                    }
+                }
+            return write_pointer;
+            }
+         
+         */
                 int i=0,j=1;
                 boolean check;
                 while(true)
