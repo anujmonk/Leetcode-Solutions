22f32cf136e867e963145d0cae838adf03920977
Anuj Sharma <74954662+anujmonk@users.noreply.github.com>
2021-12-05 16:16:32 -0500

    Time: 27 ms (34.41%), Space: 48.9 MB (64.93%) - LeetHub

---
+++ contiguous-array/contiguous-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        /** We will maintain running sum and its starting index if that running exists or if running           sum of zero exists we check for length by current index - index present in hashmap and update
+        the maximum length accordingly.
+        **/
+        
+        
+        //Creating an array to maintain running sum and its starting index
+        Map<Integer,Integer> sumMap = new HashMap<>();
+        //Updating sum of zero with index -1
+        sumMap.put(0,-1);
+        
+        int maxLength=0;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++) {
+            // Upating currSum
+            if(nums[i]==0) {
+                currSum+=-1;
+            }
+            else {
+                currSum+=1;
+            }
+            
+            // Checking for length
+            if(currSum==0) {
+                maxLength=Math.max(maxLength,i-sumMap.get(0));
+            }
+            else if(sumMap.containsKey(currSum)) {
+                maxLength=Math.max(maxLength,i-sumMap.get(currSum));
+            }
+            
+            //Updating hashmap
+            if(!sumMap.containsKey(currSum)) {
+                sumMap.put(currSum,i);
+            }    
+        }    
+        return maxLength;
+    }
+}
