9f309d63561ab7468622e85e138484b9a457ec23
Anuj Sharma <74954662+anujmonk@users.noreply.github.com>
2021-12-20 18:35:18 -0500

    Time: 53 ms (5.14%), Space: 48.8 MB (26.26%) - LeetHub

---
+++ number-of-islands/number-of-islands.java
@@ -0,0 +1,69 @@
+class Solution {
+    
+    private Set<List<Integer>> set = new HashSet<>();
+    
+    public int numIslands(char[][] grid) {
+         
+        int level=0;
+        
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if(grid[i][j]=='1') {
+                    List<Integer> list = new ArrayList<>();
+                        list.add(i);
+                        list.add(j);
+                    
+                    if(!set.contains(list)) {
+                        level = bfs(grid,i,j,level);
+                    }
+                }
+            }
+        }
+        
+    return level;
+    }
+    
+    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    
+    private int bfs(char[][] grid,int r,int c,int level) {
+        
+        
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        
+        boolean check = false;
+        while(!q.isEmpty()) {
+            
+            int[] t = q.poll();
+            
+            for(int i=0;i<dir.length;i++) {
+                
+                int rN = t[0] + dir[i][0];
+                int cN = t[1] + dir[i][1];
+                
+                List<Integer> list = new ArrayList<>() {{
+                        add(rN);
+                        add(cN);
+                    }};
+                
+                if(isValid(grid,rN,cN) && !set.contains(list) && grid[rN][cN]=='1') {
+                    
+                    set.add(list);
+                    q.add(new int[]{rN,cN});
+                }
+            }
+            
+        }
+        
+        return ++level;
+    }
+    
+    private boolean isValid(char[][] grid,int r,int c) {
+        
+        if((r>=0 && r<grid.length) && (c>=0 && c<grid[0].length)) {
+            return true;
+        }
+        return false;
+    }
+}
