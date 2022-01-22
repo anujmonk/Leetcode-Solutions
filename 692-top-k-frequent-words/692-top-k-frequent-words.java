c0c024eb7e671da5ac1a45aaa89cd50b0922f8ca
Anuj Sharma <74954662+anujmonk@users.noreply.github.com>
2022-01-21 19:29:20 -0500

    Time: 23 ms (6.11%), Space: 39.6 MB (52.11%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -8,30 +8,48 @@ class Solution {
         }
         
         
-        Queue<String> heap = new PriorityQueue<>(
-            new Comparator<String>(){
-                public int compare(String a,String b) {
-                    if(map.get(a) == map.get(b)) {
-                        return a.compareTo(b);
-                    }
-                    else {
-                        return map.get(b) - map.get(a);
-                    }
-                }
-            }
-        );
+        Queue<String> heap = new PriorityQueue<>((a,b) -> 
+                                                 {
+                                                  if(map.get(a)==map.get(b)) {
+                                                      return b.compareTo(a);
+                                                  }
+                                                  else{
+                                                     return map.get(a) - map.get(b);       
+                                                  }
+                                                 }
+                                                    );
         
         for(Map.Entry<String,Integer> entry:map.entrySet()) {
-            heap.add(entry.getKey());
+            
+            if(heap.size()<k) {
+                heap.add(entry.getKey());
+            }
+            else if(map.get(heap.peek())<=entry.getValue()) {
+                if(map.get(heap.peek())==entry.getValue()) {
+                    int a = entry.getKey().compareTo(heap.peek());
+                    
+                    System.out.println(a +" "+entry.getKey()+" "+heap.peek());
+                    if(a<0) {
+                    heap.poll();
+                    heap.add(entry.getKey()); 
+                        
+                    }
+                    continue;
+                }
+                 System.out.println(" "+entry.getKey()+" "+heap.peek());
+                heap.poll();
+                heap.add(entry.getKey());
+            }
+            
+           // System.out.println("word: "+ entry.getKey() +" "+heap);
             }
         
         
         List<String> ans = new ArrayList<>();
-        while(!heap.isEmpty() && k!=0) {
+        while(!heap.isEmpty()) {
             ans.add(heap.remove());
-            k--;
         }
-        
-        return ans;
+        Collections.reverse(ans);
+        return (ans);
     }
 }
