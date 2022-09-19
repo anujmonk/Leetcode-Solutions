f2cbdb8ee11476b81a14c441aac67b3093fe164f
Anuj Sharma <74954662+anujmonk@users.noreply.github.com>
2022-09-19 18:26:26 -0400

    Time: 32 ms (12.24%), Space: 67.7 MB (83.66%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // Without space 1. Brute force- check for every element 2. sort array check for adjacent elements
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++) {
-            if(nums[i]==nums[i+1]) {
+        //2.  Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++) {
+        //     if(nums[i]==nums[i+1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for(int i:nums) {
+            if(set.contains(i)) {
                 return true;
             }
+            set.add(i);
         }
         return false;
     }
